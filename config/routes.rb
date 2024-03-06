d80f8cea09e0a914066c5804695e7b7629ba9dc1
Cleiston Amaral <c.alexandresouza@yahoo.com>
2024-03-06 18:27:01 -0300

    condour 1 day

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :condominiums do
+    resources :posts, only: %i[new create]
+  end
+
+  resources :posts, except: %i[new create] do
+    resources :comments, only: %i[new creante destroy]
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
