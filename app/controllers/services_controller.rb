4c6eef03dbeac7c2ee9af25ec0207146df56dac4
Cleiston Amaral <c.alexandresouza@yahoo.com>
2024-03-07 16:10:39 -0300

    pages controller,view & navbar and footer

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,30 @@
 class ServicesController < ApplicationController
+
+  def index
+    @service = Service.all
+  end
+
+  def show
+    @service = Service.find(params[:id])
+  end
+
+  def new
+    @Service = Service.new
+  end
+
+  def create
+    @service = Service.new(params [:id])
+    @service.user = current_user
+    if @service.save
+      redirect_to _path(@service), notice: 'the service order was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def Service_params
+    params.require(:order).permit(:type :identification :date :time)
+  end
 end
