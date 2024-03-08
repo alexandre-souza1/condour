ce9ae22bb4fa608e0990e976044294deb945f16f
Cleiston Amaral <c.alexandresouza@yahoo.com>
2024-03-07 22:14:51 -0300

    cloudnary install

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -52,7 +52,7 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:content)
+    params.require(:post).permit(:content, photos: [])
   end
 
 end
