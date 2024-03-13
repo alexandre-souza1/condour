d27db4e00058912ddf6be3491b53ed3934202439
Lilian Garcia <lilianagng@dcc.ufrj.br>
2024-03-13 16:48:48 -0300

    edit_and_destroy_places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :set_condominium, only: [:new, :create, :destroy, :index]
+  before_action :set_condominium, only: [:new, :create, :index]
 
   def index
     @places = Place.where(condominium_id: @condominium.id).order(created_at: :desc)
@@ -11,27 +11,15 @@ class PlacesController < ApplicationController
 
   def new
     @place = Place.new
-    @condominium = Condominium.find(params[:condominium_id])
   end
 
   def create
-    @condominium = Condominium.find(params[:condominium_id])
     @place = Place.new(place_params)
     @place.condominium = @condominium
     if @place.save
-      redirect_to place_path(@place), notice: 'O local foi criado com sucesso'
+      redirect_to condominium_places_path(@condominium), notice: 'Área criada com sucesso'
     else
       render :new, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @place = Place.find(params[:id])
-    @condominium = @place.condominium
-    if @place.destroy
-      redirect_to condominium_places_path(@condominium), notice: "O local excluído com sucesso."
-    else
-      render :edit, alert: "Você não tem permissão para excluir este condomínio."
     end
   end
 
@@ -41,10 +29,20 @@ class PlacesController < ApplicationController
 
   def update
     @place = Place.find(params[:id])
+    condominium = @place.condominium
     if @place.update(place_params)
-      redirect_to place_path(@place), notice: 'o Local atualizado com sucesso'
+      redirect_to condominium_places_path(condominium), notice: 'Área atualizada com sucesso'
     else
       render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    if @place.destroy
+      redirect_to condominium_places_path(@place), notice: "Área excluída com sucesso."
+    else
+      redirect_to condominium_places_path(@place), notice: 'A área não foi excluída'
     end
   end
 
