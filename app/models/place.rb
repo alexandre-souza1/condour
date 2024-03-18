02cf22fdb73dd874161e0e80a0486a03d07c1271
andre kano <andrekano@outlook.com>
2024-03-18 09:55:47 -0300

    Validations atualizado

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,8 +4,6 @@ class Place < ApplicationRecord
 
   validates :photos, presence: true
   validates :name, presence: true
-  validates :rent_price, presence: true
   validates :residence_number, presence: true
-  validates :sale_price, presence: true
   validates :contact, presence: true
 end
